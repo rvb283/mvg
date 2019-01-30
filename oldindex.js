5c0de0c9101a11c928e0b766d68c55abe95ef5af
rvb283 <45319493+rvb283@users.noreply.github.com>
2019-01-30 16:01:45 -0700

    Add files via upload

--- oldindex.js
+++ oldindex.js
@@ -4,10 +4,10 @@ $(function(){
   let ctx = canvas.getContext('2d');
 
   let keys = {up: 87, down: 83, left: 65, right: 68, upk: 38, downk: 40, leftk: 37, rightk:39};
-  let player = {x: 0, y: 0, rpos: function(){this.x = -this.size/2; this.y = canvas.height-2*this.size/3; return this}, size:100, color:"black", speed: 20, get dSpeed(){return player.speed*(Math.sqrt(0.5));}, moveUp: false, moveDown: false, moveLeft: false, moveRight: false};
-  let goal = {x:1800, y:0, width: 200, height:200, color:"#2F2"}
-  let hborders = [{x1:0,x2:canvas.width,y:0, render:true},{x1:0,x2:canvas.width,y:canvas.height, render:true}];
-  let vborders = [{y1:0,y2:canvas.height,x:0, render:true},{y1:0,y2:canvas.height,x:canvas.width, render:true}];
+  let player = {x: 0, y: 0, rpos: function(){this.x = 0; this.y = canvas.height-this.size; return this}, size:100, color:"black", speed: 20, get dSpeed(){return player.speed*(Math.sqrt(0.5));}, moveUp: false, moveDown: false, moveLeft: false, moveRight: false};
+  let goal = {get x(){return canvas.width-goal.width}, get y(){return 0}, width: 200, height:200, color:"#2F2"}
+  let hborders = [{x1:0,x2:canvas.width,y:0, render:false},{x1:0,x2:canvas.width,y:canvas.height, render:true}];
+  let vborders = [{y1:0,y2:canvas.height,x:0, render:false},{y1:0,y2:canvas.height,x:canvas.width, render:true}];
   let levels = [
 {h:[{x1:0,x2:1800,y:800, render:true},{x1:0,x2:600,y:600, render:true},{x1:800,x2:1600,y:600, render:true},{x1:1800,x2:2000,y:400, render:true},{x1:200,x2:2000,y:400, render:true},{x1:200,x2:2000,y:200, render:true}],
 v:[{y1:600+1,y2:800-1,x:800, render:true},{y1:200, y2:600, x:1800, render:true}]},
@@ -75,17 +75,15 @@ question: "Question #1", answer:"answer",answered: false, enemies:[{x:canvas.wid
 
   function enemy(instance){
     ctx.beginPath();
-    ctx.arc(instance.x+xIncrement, instance.y+yIncrement, 50, 0, 2 * Math.PI,false);
+    ctx.arc(instance.x, instance.y, 50, 0, 2 * Math.PI,false);
     ctx.fillStyle = "red";
     ctx.lineWidth = 0;
     ctx.fill();
   }
 
   function updateEnemyPos(instance){
-    normalXPosition = -player.x;
-    normalYPosition = -player.y + 1800;
-    let targetPlayerX = (player.size/2) + normalXPosition;
-    let targetPlayerY = (player.size/2) + normalYPosition;
+    let targetPlayerX = player.x + (player.size/2);
+    let targetPlayerY = player.y + (player.size/2);
     let xFromPlayer = targetPlayerX-instance.x;
     let yFromPlayer = targetPlayerY-instance.y;
     let distanceFromPlayer = Math.sqrt(Math.pow(xFromPlayer,2)*Math.pow(yFromPlayer,2));
@@ -97,9 +95,8 @@ question: "Question #1", answer:"answer",answered: false, enemies:[{x:canvas.wid
       instance.y += Math.sin(moveAngle) * speed;
     }
   }
-
   function enemyIntersectPlayer(instance){
-    circleDistance = {x:Math.abs(instance.x - normalXPosition-(player.size/2)),y:Math.abs(instance.y - normalYPosition-(player.size/2))};
+    circleDistance = {x:Math.abs(instance.x - player.x-(player.size/2)),y:Math.abs(instance.y - player.y-(player.size/2))};
     if (circleDistance.x > (player.size/2 + 50) || circleDistance.y > (player.size/2 + 50)){
       return false;
     }
@@ -108,7 +105,6 @@ question: "Question #1", answer:"answer",answered: false, enemies:[{x:canvas.wid
     }
     cornerDistance_sq = Math.pow((circleDistance.x - player.size/2),2) + Math.pow((circleDistance.y - player.size/2),2);
     return (cornerDistance_sq <= Math.pow(50,2));
-    return false;
   }
 
   $(window).on("resize",function(){
@@ -179,70 +175,39 @@ question: "Question #1", answer:"answer",answered: false, enemies:[{x:canvas.wid
   var previousPlayerX = player.x;
   var previousPlayerY = player.y;
 
-  var xDisplacement = (canvas.width/2-player.size/2);
-  var yDisplacement = (canvas.width/2-player.size/2);
-  var xIncrement = xDisplacement+player.x;
-  var yIncrement = yDisplacement+player.y;
-  var normalXPosition = -player.x;
-  var normalYPosition = -player.y + 1800;
-
-  var accuracy = 25;
-
   function checkBorders(){
     for(let i = 0; i < hborders.length; i++){
-      if(normalXPosition < hborders[i].x2 && normalXPosition+player.size > hborders[i].x1 && normalYPosition < hborders[i].y && normalYPosition + player.size > hborders[i].y){
-        // while(normalXPosition < hborders[i].x2 && normalXPosition+player.size > hborders[i].x1 && normalYPosition < hborders[i].y && normalYPosition + player.size > hborders[i].y){
-        //   // alert("go back: " + (previousPlayerY-player.y));
-        //   alert("player now: "+normalYPosition);
-        //   player.y += previousPlayerY-player.y;
-        //   alert("player after: "+normalYPosition);
-        //   alert(normalYPosition < hborders[i].y);
-        //   alert(normalXPosition < hborders[i].x2 && normalXPosition+player.size > hborders[i].x1 && normalYPosition < hborders[i].y && normalYPosition + player.size > hborders[i].y);
-        // }
+      if(player.x < hborders[i].x2 && player.x+player.size > hborders[i].x1 && player.y < hborders[i].y && player.y + player.size > hborders[i].y){
         player.y = previousPlayerY;
-
-        // alert("player y is now "+player.y);
       }
-      if(normalYPosition < hborders[i].y && normalYPosition+player.size > hborders[i].y && normalXPosition < hborders[i].x2 && normalXPosition+player.size > hborders[i].x1){
+      if(player.y < hborders[i].y && player.y+player.size > hborders[i].y && player.x < hborders[i].x2 && player.x+player.size > hborders[i].x1){
         player.x = previousPlayerX;
       }
     }
     for(let i = 0; i < vborders.length; i++){
-      if(normalXPosition <= vborders[i].x && normalXPosition+player.size > vborders[i].x && normalYPosition < vborders[i].y2 && normalYPosition + player.size > vborders[i].y1){
+      if(player.x <= vborders[i].x && player.x+player.size > vborders[i].x && player.y < vborders[i].y2 && player.y + player.size > vborders[i].y1){
         player.x = previousPlayerX;
       }
-      if(normalYPosition < vborders[i].y2 && normalYPosition+player.size > vborders[i].y1 && vborders[i].x > normalXPosition && vborders[i].x < normalXPosition+player.size){
+      if(player.y < vborders[i].y2 && player.y+player.size > vborders[i].y1 && vborders[i].x > player.x && vborders[i].x < player.x+player.size){
         player.y = previousPlayerY;
       }
     }
   }
-
-
   //Game Loop
   function loop(t){
     //Background
     rect(0, 0, canvas.width, canvas.height, "white");
-    // console.log("Previous Player X:"+previousPlayerX+"Previous Player Y"+previousPlayerX);
-
-    normalXPosition = -player.x;
-    normalYPosition = -player.y + 1800;
-
-    xIncrement = (canvas.width/2-player.size/2)+player.x;
-    yIncrement = (-3*canvas.height/2+3*player.size/2)+player.y;
 
     previousPlayerX = player.x;
     previousPlayerY = player.y;
-
+    let accuracy = 25;
     if(player.moveUp/* && !hasTB*/){
       for(j = 0; j < accuracy; j++){
-        // alert("before y increase:"+player.y);
         if(!player.moveLeft && !player.moveRight){
-          player.y += player.speed/accuracy;
+          player.y -= player.speed/accuracy;
         } else{
-          player.y += player.dSpeed/accuracy;
+          player.y -= player.dSpeed/accuracy;
         }
-        normalXPosition = -player.x;
-        normalYPosition = -player.y + 1800;
         checkBorders();
         previousPlayerY = player.y;
       }
@@ -250,12 +215,10 @@ question: "Question #1", answer:"answer",answered: false, enemies:[{x:canvas.wid
     if(player.moveDown/* && !hasBB*/){
       for(j = 0; j < accuracy; j++){
         if(!player.moveLeft && !player.moveRight){
-          player.y -= player.speed/accuracy;
+          player.y += player.speed/accuracy;
         } else{
-          player.y -= player.dSpeed/accuracy;
+          player.y += player.dSpeed/accuracy;
         }
-        normalXPosition = -player.x;
-        normalYPosition = -player.y + 1800;
         checkBorders();
         previousPlayerY = player.y;
       }
@@ -263,12 +226,10 @@ question: "Question #1", answer:"answer",answered: false, enemies:[{x:canvas.wid
     if(player.moveLeft/* && !hasLB*/){
       for(j = 0; j < accuracy; j++){
         if(!player.moveUp && !player.moveDown){
-          player.x += player.speed/accuracy;
+          player.x -= player.speed/accuracy;
         } else{
-          player.x += player.dSpeed/accuracy;
+          player.x -= player.dSpeed/accuracy;
         }
-        normalXPosition = -player.x;
-        normalYPosition = -player.y + 1800;
         checkBorders();
         previousPlayerX = player.x;
       }
@@ -276,48 +237,37 @@ question: "Question #1", answer:"answer",answered: false, enemies:[{x:canvas.wid
     if(player.moveRight/* && !hasRB*/){
       for(j = 0; j < accuracy; j++){
         if(!player.moveUp && !player.moveDown){
-          player.x -= player.speed/accuracy;
+          player.x += player.speed/accuracy;
         } else{
-          player.x -= player.dSpeed/accuracy;
+          player.x += player.dSpeed/accuracy;
         }
-        normalXPosition = -player.x;
-        normalYPosition = -player.y + 1800;
         checkBorders();
         previousPlayerX = player.x;
       }
     }
-
-    newPlayerDifferenceX = player.x-previousPlayerX;
-    newPlayerDifferenceY = previousPlayerY-player.y;
 
     // let hasTB = false, hasBB = false, hasLB = false, hasRB=false;
     /*Render*/
 
     for(let i = 0; i < hborders.length; i++){
       if(hborders[i].render){
-        ctx.beginPath();
-        ctx.moveTo(hborders[i].x1+(xIncrement), hborders[i].y+(yIncrement));
-        ctx.lineTo(hborders[i].x2+(xIncrement), hborders[i].y+(yIncrement));
-        ctx.stroke();
+        rect(hborders[i].x1,hborders[i].y,(hborders[i].x2-hborders[i].x1),2,"black");
       }
     }
 
     for(let i = 0; i < vborders.length; i++){
       if(vborders[i].render){
-        ctx.beginPath();
-        ctx.moveTo(vborders[i].x+(xIncrement), vborders[i].y1+(yIncrement));
-        ctx.lineTo(vborders[i].x+(xIncrement), vborders[i].y2+(yIncrement));
-        ctx.stroke();
+        rect(vborders[i].x,vborders[i].y1,2,(vborders[i].y2-vborders[i].y1),"black");
       }
     }
 
     $("#score").text(level);
 
     //Goal
-    rect(goal.x + xIncrement, goal.y + yIncrement, goal.width, goal.height, goal.color);
+    rect(goal.x, goal.y, goal.width, goal.height, goal.color);
 
     //Player
-    rect(canvas.width/2 - player.size/2, canvas.height/2- player.size/2, player.size, player.size, player.color);
+    rect(player.x, player.y, player.size, player.size, player.color);
 
     enemyArray.forEach(updateEnemyPos);
     enemyArray.forEach(enemy);
@@ -327,19 +277,10 @@ question: "Question #1", answer:"answer",answered: false, enemies:[{x:canvas.wid
       }
     });
 
-    if(normalXPosition <= goal.x + goal.width && normalXPosition + player.size >= goal.x && normalYPosition + player.size >= goal.y &&  normalYPosition <= goal.y + goal.height){
+    if(player.x <= goal.x + goal.width && player.x + player.size >= goal.x && player.y + player.size >= goal.y && player.y <= goal.y + goal.height){
       level++;
       loadLevel();
     }
-    // console.log("xIncrement: "+xIncrement +" yIncrement: "+yIncrement);
-    // console.log("xDisplacement: "+xDisplacement +" yDisplacement: "+yDisplacement);
-    // console.log("Player X: "+player.x +" Player Y: "+player.y);
-    // console.log("Normal X:"+normalXPosition+"Normal Y: "+normalYPosition);
-    // console.log("Goal X: "+goal.x +" Goal Y: "+goal.y);
-    // console.log("-player.x <= goal.x + goal.width + xDisplacement " + (-player.x <= goal.x + goal.width));
-    // console.log("-player.x + player.size >= goal.x + xDisplacement " + (-player.x + player.size >= goal.x));
-    // console.log("-player.y+1800 + player.size >= goal.y - yDisplacement" + (-player.y + 1800 + player.size >= goal.y));
-    // console.log("-player.y+1800 <= goal.y + goal.height - yDisplacement" + ( -player.y + 1800 <= goal.y + goal.height));
 
     window.requestAnimationFrame(loop);
   }
